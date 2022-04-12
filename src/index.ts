836f0570e82687f4f36c0e9221c5aac01b98f996
Javier Ruiz <80712191+athjavi@users.noreply.github.com>
2022-04-12 19:47:55 +0200

    MAIN HTML

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ const app = express()
 app.use(express.json())
 const PORT = process.env.PORT || 3000
 
+app.get('/', (_req, res) => {
+    res.send("MAIN HTML");
+    console.log("Pinged : " + new Date().toLocaleDateString())
+    res.send("pong: " + new Date().toLocaleDateString())
+})
+
 app.get('/ping', (_req, res) => {
     console.log("Pinged : " + new Date().toLocaleDateString())
     res.send("pong: " + new Date().toLocaleDateString())
