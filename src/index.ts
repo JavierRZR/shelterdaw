70a2cc77133f063cee1686e5288552547b775a11
Javier Ruiz <80712191+athjavi@users.noreply.github.com>
2022-04-22 15:14:02 +0200

    Creación de primeras plantillas, inacabadas, para ver pruebas en hosting.

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,29 @@ import express from 'express'
 
 const app = express()
 app.use(express.json())
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+
 const PORT = process.env.PORT || 3000
 
 app.get('/', (_req, res) => {
-    res.send("MAIN HTML");
-    console.log("Pinged : " + new Date().toLocaleDateString())
-    res.send("pong: " + new Date().toLocaleDateString())
+    res.render('index.pug', {
+        name: "Prueba",
+        parrafo: "Esto e sun parrafo"
+    })
 })
 
 app.get('/ping', (_req, res) => {
     console.log("Pinged : " + new Date().toLocaleDateString())
     res.send("pong: " + new Date().toLocaleDateString())
 })
-//Prueba
+
+app.get('/protectora', (_req, res) => {
+    res.render('protectora.pug', {
+        content: 'protectoras.pug'
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port aaaa ${PORT}`)
 })
- //asdsa
